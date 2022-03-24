dc843a56335834bd6b7848affd3d2c24719ea9f3
getbharat <linkedin.bharat@gmail.com>
2022-03-24 05:34:54 -0500

    fix accept header issue and make use of k8s for deployment (#1)
    * fix accept header issue and containerize the services
    
    * Use enviroment varaible to access order - ClusterIP service
    
    * Add k8s deployment and make use of ClusterIP service

--- customer/src/main/java/com/ecom/customer/CustomerApplication.java
+++ customer/src/main/java/com/ecom/customer/CustomerApplication.java
@@ -2,8 +2,10 @@ package com.ecom.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(scanBasePackages = {"com.ecom.customer"})
+@Configuration
 public class CustomerApplication {
 
 	public static void main(String[] args) {
--- customer/src/main/java/com/ecom/customer/rest/CustomerController.java
+++ customer/src/main/java/com/ecom/customer/rest/CustomerController.java
@@ -1,24 +1,54 @@
 package com.ecom.customer.rest;
 
 import com.ecom.customer.models.Customer;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-@RestController("/customer")
+@RestController
 public class CustomerController {
 
-    @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Customer>> getCustomers(){
+    private static String BASE_URL = "http://order:8091/order" ;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping(value = "/get", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XHTML_XML_VALUE, MediaType.TEXT_XML_VALUE,MediaType.TEXT_PLAIN_VALUE, MediaType.TEXT_HTML_VALUE}, consumes = {"*/*"} )
+    @ResponseBody
+    public List<Customer> getCustomers(){
         final List<Customer> customerList = new ArrayList<>();
         customerList.add(new Customer("C1", "GE", "New York, US"));
         customerList.add(new Customer("C2", "Reynolds", "New York, US"));
         customerList.add(new Customer("C3", "Thomson Reuters", "Toronto, Canada"));
 
-        return ResponseEntity.ok(customerList);
+        return customerList;
+    }
+
+    @GetMapping(value = "/get/order/{customerId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XHTML_XML_VALUE, MediaType.TEXT_XML_VALUE,MediaType.TEXT_PLAIN_VALUE, MediaType.TEXT_HTML_VALUE}, consumes = {"*/*"} )
+    @ResponseBody
+    public Object[] getOrderByCustomerId(@PathVariable("customerId") final String customerId){
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_XHTML_XML, MediaType.TEXT_XML, MediaType.TEXT_PLAIN, MediaType.TEXT_HTML));
+        final HttpEntity<Object> entity = new HttpEntity<>(headers);
+        final Map<String, String> pathParams = new HashMap<>();
+        pathParams.put("customerId", customerId);
+        if(environment != null && environment.getProperty("order") != null){
+            BASE_URL = environment.getProperty("order")+"/order";
+        }
+        final String url = UriComponentsBuilder.fromHttpUrl(BASE_URL).path("/get/{customerId}").buildAndExpand(pathParams).encode().toUriString();
+        final ResponseEntity<Object[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, Object[].class);
+        final Object[] objects = responseEntity.getBody();
+        return objects ;
     }
 }
